d87c62cfa22b427ba0dacdf2ba8b378e7b88a922
alexandrej01 <alexandrej_01@hotmail.com>
2019-02-08 11:44:27 -0200

    Tradução PT-BR Adcionada

---
+++ resources/lang/pt/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'O usuário ou senha informados não constam na base de dados.',
+    'throttle' => 'Muitas tentativas consecutivas de login. Tente novamente mais tarde.',
+
+];
---
+++ resources/lang/pt/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'A senha deve conter mais de seis digitos e as duas senhas devem ser identicas.',
+    'reset' => 'Sua senha foi alterada!',
+    'sent' => 'Enviamos no seu email um link de alteração!',
+    'token' => 'Este token de confirmação não é válido.',
+    'user' => "Email não encontrado na base de dados.",
+
+];
