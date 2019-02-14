6741d0d5f82c8240ad5a5a3e63a435e964100514
alexandrej01 <alexandrej_01@hotmail.com>
2019-02-14 16:55:47 -0200

    Change -- Melhoria de rotas

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,35 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Tela inicial -- Sem auth
+ Route::get('/', function () {return view('welcome');});
 
-Auth::routes();
+//Básico
 
-Route::get('activate/{token}', 'Auth\RegisterController@activate')
-    ->name('activate');
+// Rotas de Autenticação
+ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+ Route::post('login', 'Auth\LoginController@login');
+ Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::get('/dashboard', 'ScreenController@dashboard')->name('dash');
 
-Route::get('/users', 'ScreenController@users')->name('users');
+// Rota de Registro.
+ Route::post('register', 'Auth\RegisterController@register')->name('users.register');
+ Route::get('activate/{token}', 'Auth\RegisterController@activate');
+
+
+
+// Rotas de Recuperação de Senha.
+ Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+ Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+
+//Dashboard
+ Route::get('/dashboard', 'ScreenController@dashboard')->name('dash');  //Dashb
+
+
+//Controle de Usuários --Cadastrar/Excluir/Editar
+ Route::resource('/users', 'App\UsersController');
+ Route::post('update', 'App\UsersController@update')->name('users.update');
+ Route::post('destroy', 'App\UsersController@destroy')->name('users.destroy');
